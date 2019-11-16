670e8479ac3af04f69779878398cec493e343959
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-15 21:48:10 -0300

    Implementing action related to button.

--- app/src/main/java/com/cursoandroid/idadedecachorro/MainActivity.java
+++ app/src/main/java/com/cursoandroid/idadedecachorro/MainActivity.java
@@ -3,6 +3,7 @@ package com.cursoandroid.idadedecachorro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_descobrirIdade;
     private TextView txt_resultadoIdade;
 
+    private String entradaUsuario;
+    private int valorDeEntrada;
+    private int resultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,26 @@ public class MainActivity extends AppCompatActivity {
         txt_idade = findViewById(R.id.txt_idade);
         btn_descobrirIdade = findViewById(R.id.btn_descobrirIdade);
         txt_resultadoIdade = findViewById(R.id.txt_resultadoIdade);
+
+        btn_descobrirIdade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                entradaUsuario = txt_idade.getText().toString();
+
+                if (!entradaUsuario.isEmpty()){
+                    valorDeEntrada = Integer.parseInt(entradaUsuario);
+                    resultado = valorDeEntrada * 7;
+
+                    txt_resultadoIdade.setText(getString(R.string.resultado) + " " + resultado);
+
+                }
+                else {
+                    //enviar mensagem de erro
+                    txt_resultadoIdade.setText(R.string.erro);
+                }
+            }
+        });
 
     }
 }
